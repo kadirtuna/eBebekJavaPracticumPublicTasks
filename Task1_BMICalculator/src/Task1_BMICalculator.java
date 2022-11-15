a2984d01f148fd0a51da01b670c4c14750931510
magique <kadirbey832832@gmail.com>
2022-11-15 15:33:18 +0300

    For SOLID Principles
    Change in layout.

---
+++ Task1_BMICalculator/src/Task1_BMICalculator.java
@@ -0,0 +1,22 @@
+/**
+ * Title: Task1_BMICalculator class
+ * Author: Kadir Tuna
+ * Assignment: eBebek Java & QA & SAP Spartacus Practicum
+ * Description: This class is for calculating Body Mass Index of a human with given height and weight of him.
+ * Contacts;
+ * Instagram: kadir.tuna
+ * Github : github.com/kadirtuna
+ * LinkedIn : linkedin.com/in/kadirtuna20
+ *
+ * by;
+ * @author Kadir Tuna
+ */
+
+import java.util.Scanner;
+
+public class Task1_BMICalculator {
+
+    public static float BMICalculate(float height, float weight){
+        return weight / (height * height);
+    }
+}
--- Task1_BMICalculator/src/Task1_BMICalculatorMain.java
+++ Task1_BMICalculator/src/Task1_BMICalculatorMain.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class Task1_VucutKitleEndeksiHesaplama {
+public class Task1_BMICalculatorMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : ");
         float height = scan.nextFloat();
         System.out.print("Lütfen kilonuzu giriniz : ");
         float weight = scan.nextFloat();
-        System.out.println("Vücut Kitle İndeksiniz : " + weight / (height * height));
+        System.out.println("Vücut Kitle İndeksiniz : " + Task1_BMICalculator.BMICalculate(height, weight));
     }
 }
